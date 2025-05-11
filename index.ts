1b22ab08def0ef1cb7c41ea078d106ae86155f23
Samarth Chaplot <samarthchaplot7@gmail.com>
2025-05-11 20:35:34 +0530

    Added Fixes

--- index.ts
+++ index.ts
@@ -59,7 +59,7 @@ app.use(
 			return callback(null, true);
 		},
 		credentials: true,
-		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"], // Added PATCH
 		allowedHeaders: [
 			"Origin",
 			"X-Requested-With",
